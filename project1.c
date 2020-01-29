6eb1464df91a9aa56b496ef5742021240fab2e66
B3brooks5 <45647104+B3brooks@users.noreply.github.com>
2020-01-28 21:36:21 -0500

    Restructuring
    changes order of functions to be easier to follow

--- project1.c
+++ project1.c
@@ -24,37 +24,35 @@ void printTokens(instruction* instr_ptr);
 void clearInstruction(instruction* instr_ptr);
 void addNull(instruction* instr_ptr);
 
-
-
-char* echo(instruction* cmd );
-
 void interpret(instruction* instr_ptr, char* PWD, char** backProc, pid_t** id, int size,
     int* current, int count);	// main function called once every instruction
+// echo
+char* echo(instruction* cmd );
 
-
-char** resizeArray(char**, int*);	// returns an array with double the size, doubles passed int
-char** createArray(int);		// initiates a new 2d dynamically allocated array
-void deallocateArray(char**, int); 	// destructs a dynamically allocated array
+// path finding
 char* parse_path(char* str, const char* PWD);	// gets absolute path of one entered
 void double_period(char* ret);		// help parse double period
-int path_check(const char* path);	// cheaks if path namne valid
+int path_check(const char* path);	// checks if path name valid
+
+// execute
 char* commandPath(char* cmd);		//returns command added to correct $PATH location
 void my_execute(char** cmd);			//executes commands
-char* resizeTeacher(char* his, char* ours);	// resizes origional null terminated array of strings
+void check_instruction_paths(instruction* instr,
+    const char* PWD);	// makes sure instructions are valid
+void execute_found_instruction(instruction* instr, int * inDirect, int * outDirect, int * pipe,
+	int* background, int* outIndex, int* inIndex, char** PWD);// decides what instruction to execute
 
 // redirection
 int output_redirect(char** cmd, int index, int bg);
 int input_redirect(char**cmd, int index, int bg);
 int in_out_redirect(char** cmd, int in, int out, int bg);
-void check_instruction_paths(instruction* instr,
-    const char* PWD);	// makes sure instructions are valid
 int redirect_check(char** cmd, int index);		// makes sure file paths are correct
-void execute_found_instruction(instruction* instr, int * inDirect, int * outDirect, int * pipe,
-	int* background, int* outIndex, int* inIndex, char** PWD);// decides what instruction to execute
+
 // Piping
 int piping(char** cmd, int index, int bg);
 int pipe_check(char** cmd, int index, int index2);	// cheaks that pipe tokens and paths are valid
 int double_pipe(char** cmd, int index1, int index2, int bg);	// for two pipes, doesn't work
+
 // backgound processing
 void is_background(instruction* instr_ptr, char** backProc,
     pid_t** id, int size, int* current);//determine if background or foreground process
@@ -67,7 +65,11 @@ char** makeCopy(instruction* instr_ptr);	//makes copy without the &
 pid_t** idArray(int size);	                //creates id array
 pid_t** resizeId(pid_t** array, int* sizeofarray);
 
-
+// other functions
+char* resizeTeacher(char* his, char* ours);	// resize original null terminated array of strings
+char** resizeArray(char**, int*);	// returns an array with double the size, doubles passed int
+char** createArray(int);		// initiates a new 2d dynamically allocated array
+void deallocateArray(char**, int); 	// destructs a dynamically allocated array
 int main() {
 	char* token = NULL;
 	char* temp = NULL;
@@ -205,7 +207,7 @@ void check_instruction_paths(instruction* instr, const char* PWD){
 	{	// i for counter, null for the end
 		if ((instr->tokens)[i] != NULL)
 		{
-			if (strstr(instr->tokens[i],"/") != NULL || strstr(instr->tokens[i],".") != NULL || 
+			if (strstr(instr->tokens[i],"/") != NULL || strstr(instr->tokens[i],".") != NULL ||
 			    strstr(instr->tokens[i],"~") != NULL)
 			{
 				char * temp = (char*)malloc(500 * sizeof(char));
@@ -225,7 +227,7 @@ void check_instruction_paths(instruction* instr, const char* PWD){
 }
 
 // return value of 1 for each instruction identified
-int check_instruction_type(instruction* instr, int * inDirect, 
+int check_instruction_type(instruction* instr, int * inDirect,
     int * outDirect, int * pipe, int * background, int * outIndex, int * inIndex){
 	int i = 0;
 	for (i = 0; i < instr->numTokens; i++)
@@ -268,7 +270,7 @@ int check_instruction_type(instruction* instr, int * inDirect,
 
 
 // read through tokens
-void interpret(instruction* instr_ptr, char* PWD, char** backProc, 
+void interpret(instruction* instr_ptr, char* PWD, char** backProc,
     pid_t** id, int size, int* current, int count) {
 	if(strcmp(instr_ptr->tokens[0], "echo") == 0) {		// get custom environment variabe string
 		printf("%s\n", echo(instr_ptr));
@@ -288,7 +290,7 @@ void interpret(instruction* instr_ptr, char* PWD, char** backProc,
 
 	int in_redirect = 0, out_redirect = 0, pipe = 0, background = 0, out_index = 0, in_index = 0;
 
-	if (check_instruction_type(instr_ptr, &in_redirect, &out_redirect, 
+	if (check_instruction_type(instr_ptr, &in_redirect, &out_redirect,
 	    &pipe, &background, &out_index, &in_index) == 1){
 		printf("Unknown instruction type\n"); // if tokens not in a valid order
 		return;
@@ -298,7 +300,7 @@ void interpret(instruction* instr_ptr, char* PWD, char** backProc,
                 is_background(instr_ptr, backProc, id, size, current);
         }
 	else if (in_redirect == 1 && out_redirect == 1){
-		if( redirect_check(instr_ptr->tokens, in_index) == 1 && 
+		if( redirect_check(instr_ptr->tokens, in_index) == 1 &&
 		    redirect_check(instr_ptr->tokens, out_index) == 1)
 			in_out_redirect(instr_ptr->tokens, in_index, out_index, 0);
 		else
@@ -331,7 +333,7 @@ void interpret(instruction* instr_ptr, char* PWD, char** backProc,
 	{
 		if (strcmp(instr_ptr->tokens[1],".") == 0)
 		{}
-		else if(strstr(instr_ptr->tokens[1],"/") == NULL && 
+		else if(strstr(instr_ptr->tokens[1],"/") == NULL &&
 		    strstr(instr_ptr->tokens[1],"..") == NULL)
 		{	//if there is not a / and not a ..
 			char * tempPWD = (char*)malloc(500 * sizeof(char));
@@ -380,6 +382,36 @@ void interpret(instruction* instr_ptr, char* PWD, char** backProc,
 
 }
 
+            /* echo */
+char* echo(instruction* cmd){
+	int i = 1;
+	char* ret = (char*)malloc(500 * sizeof(char));
+	if(strcmp(cmd->tokens[0], "echo") == 0){
+		while(cmd->tokens[i] != NULL){
+			if(cmd->tokens[i][0] == '$' && isalpha(cmd->tokens[i][1]) != 0 ){
+				// see if it is an environment variable
+				char* env = getenv(&(cmd->tokens[i][1]));
+				if(env != NULL){	// if env var found
+					strcat(ret, env);
+					strcat(ret, " ");
+				}
+				else {			// else error
+					strcpy(ret, cmd->tokens[i]);
+					strcat(ret, ": Undefined variable");
+					break;
+				}
+			}
+			else {				// if not env just add to ret
+				strcat(ret, cmd->tokens[i]);
+				strcat(ret, " ");
+			}
+		i++;
+		}
+	}
+	return ret;	// return entire string
+}
+
+            /* path finding */
 // gets absolute path returned at string
 char* parse_path(char* path, const char* PWD){
 	char** array;	// 2D array
@@ -474,119 +506,30 @@ void double_period(char* ret){
 	}
 }
 
-// releases memory of dynamically allocated array
-void deallocateArray(char** arr, int size) {
-	int i;
-	for (i = 0; i < size; i++)
-		free(arr[i]);
-
-	free(arr);
-}
-
-// create new dynamic array of size 'size'
-char** createArray(int size) {
-	char** newarr = (char**)malloc(size * sizeof(char*));	// size of array
-
-	int i;
-	for (i = 0; i < size; i++) 			// size of each index
-		newarr[i] = (char*)malloc(100 * sizeof(char));
 
 
-	return newarr;
-}
+            /* execution */
+void my_execute(char** cmd)
+{	char* path = (char*)malloc(500 * sizeof(char));
+	strcpy(path, commandPath(cmd[0]));
+	int status;
+	pid_t pid = fork();
 
-// resizes an array and changes the size to double the passed size
-char** resizeArray(char** array, int* sizeofarray) {
-
-	// keep origional size for dealocation, double it for new array
-	int oldsize = *sizeofarray;
-	*sizeofarray = oldsize * 2;
-
-	// create new array
-	char ** new = (char**)malloc(*sizeofarray * sizeof(char*));
-
-	// allocate each index of new array
-	int i;
-	for (i = 0; i < *sizeofarray; i++)
-		new[i] = (char*)malloc(100 * sizeof(char));
-
-
-	// copy contents from old to new
-	for (i = 0; i < oldsize; i++)
-		strcpy(new[i], array[i]);
-
-
-	// free old array
-	for (i = 0; i < oldsize; i++)
-		free(array[i]);
-
-	free(array);
-
-	return new;
-}
-
-pid_t** idArray(int size)                      //creates id array
-{
-	pid_t** newarr = (pid_t**)malloc(size * sizeof(pid_t*));
-        int i;
-        for (i = 0; i < size; i++) {           //allocates each index in the array
-                newarr[i] = (pid_t*)malloc(100 * sizeof(pid_t));
-        }
-        return newarr;
-}
-
-pid_t** resizeId(pid_t** array, int* sizeofarray) 
-{
-	//keep origional size for dealocation, double it for new array
-	int oldsize = *sizeofarray;
-        *sizeofarray = oldsize * 2;
-	//create new array
-	pid_t ** new = (pid_t**)malloc(*sizeofarray * sizeof(pid_t*));
-	//allocate each index of new array
-	int i;
-        for (i = 0; i < *sizeofarray; i++) {
-                new[i] = (pid_t*)malloc(100 * sizeof(pid_t));
-        }
-	//copy contents from old to new
-	for (i = 0; i < oldsize; i++) {
-        //        strcpy(new[i], array[i]);
-        	new[i] = array[i];
+	if (pid == -1)		//error
+	{
+		printf("Error");
+		exit(1);
 	}
-	//free old array
-	for (i = 0; i < oldsize; i++) {
-                free(array[i]);
-        }
-        free(array);
-
-        return new;
-}
-
-char* echo(instruction* cmd){
-	int i = 1;
-	char* ret = (char*)malloc(500 * sizeof(char));
-	if(strcmp(cmd->tokens[0], "echo") == 0){
-		while(cmd->tokens[i] != NULL){
-			if(cmd->tokens[i][0] == '$' && isalpha(cmd->tokens[i][1]) != 0 ){
-				// see if it is an environment variable
-				char* env = getenv(&(cmd->tokens[i][1]));
-				if(env != NULL){	// if env var found
-					strcat(ret, env);
-					strcat(ret, " ");
-				}
-				else {			// else add string
-					strcpy(ret, cmd->tokens[i]);
-					strcat(ret, ": Undefined variable"); 
-					break;
-				}
-			}
-			else {				// if noe env just add is
-				strcat(ret, cmd->tokens[i]);
-				strcat(ret, " ");
-			}
-		i++;
-		}
+	else if (pid == 0)	//child
+	{
+		execv(path, cmd);
+		printf("Problem executing %s\n", cmd[0]);
+		exit(1);
 	}
-	return ret;	// return entire string
+	else			//parent
+	{
+		waitpid(pid, &status, 0);
+	}
 }
 
 char* commandPath(char* cmd)
@@ -613,27 +556,49 @@ char* commandPath(char* cmd)
 	return temp;
 }
 
-void my_execute(char** cmd)
-{	char* path = (char*)malloc(500 * sizeof(char));
-	strcpy(path, commandPath(cmd[0]));
-	int status;
-	pid_t pid = fork();
+            /* I/O redirection */
+int input_redirect(char** cmd, int index, int bg){
+	char* file = cmd[index + 1]; // grab the name of the file
+	int fd = open(file, O_RDONLY);
+	int status, i = 0;
 
-	if (pid == -1)		//error
-	{
-		printf("Error");
-		exit(1);
+	instruction Myinstr;		// create our own null terminated list
+	Myinstr.tokens = NULL;
+	Myinstr.numTokens = 0;
+
+	for (i = 0; i < index; i++){	// copy up in 'index' where the > is located
+		char * temp = (char*)malloc((strlen(cmd[i]) + 1) * sizeof(char));
+		strcpy(temp, cmd[i]);
+		addToken(&Myinstr, temp);
 	}
-	else if (pid == 0)	//child
-	{
-		execv(path, cmd);
-		printf("Problem executing %s\n", cmd[0]);
-		exit(1);
+	addNull(&Myinstr);		// amke null terminated
+
+	pid_t pid = fork();		// begin fork
+	if (pid == -1){
+		printf("Error opening file\n");
 	}
-	else			//parent
+	else if(pid == 0){		// child
+		close(0);
+		dup(fd);
+		close(fd);
+
+		char * path = commandPath(cmd[0]);
+
+		execv(path, Myinstr.tokens);
+
+		printf("Problem executing\n");
+	}
+	else if (bg == 1)		//if background
 	{
+		waitpid(pid, &status, WNOHANG);
+                close(fd);
+	}
+	else {				// parent
 		waitpid(pid, &status, 0);
+		close(fd);
 	}
+	clearInstruction(&Myinstr);
+	return pid;
 }
 
 int output_redirect(char** cmd, int index, int bg){
@@ -668,50 +633,6 @@ int output_redirect(char** cmd, int index, int bg){
 		printf("Problem executing\n");
 	}
 	else if (bg == 1)		//background processing
-	{
-		waitpid(pid, &status, WNOHANG);
-                close(fd);
-	}
-	else {				// parent
-		waitpid(pid, &status, 0);
-		close(fd);
-	}
-	clearInstruction(&Myinstr);
-	return pid;
-}
-
-int input_redirect(char** cmd, int index, int bg){
-	char* file = cmd[index + 1]; // grab the name of the file
-	int fd = open(file, O_RDONLY);
-	int status, i = 0;
-
-	instruction Myinstr;		// create our own null terminated list
-	Myinstr.tokens = NULL;
-	Myinstr.numTokens = 0;
-
-	for (i = 0; i < index; i++){	// copy up in 'index' where the > is located
-		char * temp = (char*)malloc((strlen(cmd[i]) + 1) * sizeof(char));
-		strcpy(temp, cmd[i]);
-		addToken(&Myinstr, temp);
-	}
-	addNull(&Myinstr);		// amke null terminated
-
-	pid_t pid = fork();		// begin fork
-	if (pid == -1){
-		printf("Error opening file\n");
-	}
-	else if(pid == 0){		// child
-		close(0);
-		dup(fd);
-		close(fd);
-
-		char * path = commandPath(cmd[0]);
-
-		execv(path, Myinstr.tokens);
-
-		printf("Problem executing\n");
-	}
-	else if (bg == 1)		//if background
 	{
 		waitpid(pid, &status, WNOHANG);
                 close(fd);
@@ -800,21 +721,7 @@ int redirect_check(char** cmd, int index){
 	return 0;
 }
 
-int pipe_check(char** cmd, int index, int index2){
-	if(path_check(cmd[0]) == 1){
-		if(index2 != 0){	// more then one pipe
-			if(path_check(cmd[index + 1]) == 1 && path_check(cmd[index2 +1]) == 1)
-				return 2;	// valid call for two pipes
-		}
-		else {
-			if(path_check(cmd[index +1]) == 1){
-				return 1;
-			}
-		}
-	}
-	return 0;
-}
-
+            /* Piping functions */
 int piping(char** cmd, int index, int bg) {
 	instruction command1;		// create our own null terminated list
 	command1.tokens = NULL;
@@ -883,7 +790,6 @@ int piping(char** cmd, int index, int bg) {
 	clearInstruction(&command2);
 	return pid;
 }
-
 
 int double_pipe(char** cmd, int index1, int index2, int bg){
 	instruction command0;		// create our own null terminated list
@@ -995,19 +901,24 @@ int double_pipe(char** cmd, int index1, int index2, int bg){
 	return pid;
 }
 
-
-
-
-char* resizeTeacher(char* his, char* ours) // will resize the current in instruction struct
-{
-	free(his);
-        his = (char*)malloc(strlen(ours) * sizeof(char));
-        his = ours;
-        ours = NULL;
-	return his;
+int pipe_check(char** cmd, int index, int index2){
+	if(path_check(cmd[0]) == 1){
+		if(index2 != 0){	// more then one pipe
+			if(path_check(cmd[index + 1]) == 1 && path_check(cmd[index2 +1]) == 1)
+				return 2;	// valid call for two pipes
+		}
+		else {
+			if(path_check(cmd[index +1]) == 1){
+				return 1;
+			}
+		}
+	}
+	return 0;
 }
 
-void is_background(instruction* instr_ptr, char** backProc, pid_t** id, 
+
+            /* Background Processing */
+void is_background(instruction* instr_ptr, char** backProc, pid_t** id,
     int size, int* current) //determine if background or foreground process
 {
 	if (strcmp(instr_ptr->tokens[0],"&") == 0)					//if & CMD
@@ -1092,7 +1003,7 @@ void is_background(instruction* instr_ptr, char** backProc, pid_t** id,
 			}
 			else if (in_redirect == 1 && out_redirect == 1)
 			{
-                		if( redirect_check(instr_ptr->tokens, in_index) == 1 && 
+                		if( redirect_check(instr_ptr->tokens, in_index) == 1 &&
                 		    redirect_check(instr_ptr->tokens, out_index) == 1)
                         	{
 					*id[*current] = in_out_redirect(instr_ptr->tokens, in_index, out_index,1);
@@ -1133,6 +1044,42 @@ void is_background(instruction* instr_ptr, char** backProc, pid_t** id,
 	}
 	else
 		printf("Error: Invalid Syntax\n");
+}
+
+pid_t** idArray(int size)                      //creates id array
+{
+	pid_t** newarr = (pid_t**)malloc(size * sizeof(pid_t*));
+        int i;
+        for (i = 0; i < size; i++) {           //allocates each index in the array
+                newarr[i] = (pid_t*)malloc(100 * sizeof(pid_t));
+        }
+        return newarr;
+}
+
+pid_t** resizeId(pid_t** array, int* sizeofarray)
+{
+	//keep origional size for dealocation, double it for new array
+	int oldsize = *sizeofarray;
+        *sizeofarray = oldsize * 2;
+	//create new array
+	pid_t ** new = (pid_t**)malloc(*sizeofarray * sizeof(pid_t*));
+	//allocate each index of new array
+	int i;
+        for (i = 0; i < *sizeofarray; i++) {
+                new[i] = (pid_t*)malloc(100 * sizeof(pid_t));
+        }
+	//copy contents from old to new
+	for (i = 0; i < oldsize; i++) {
+        //        strcpy(new[i], array[i]);
+        	new[i] = array[i];
+	}
+	//free old array
+	for (i = 0; i < oldsize; i++) {
+                free(array[i]);
+        }
+        free(array);
+
+        return new;
 }
 
 char* commandLine(instruction* instr_ptr)	// returns all the tokens for that lines as one string
@@ -1200,7 +1147,7 @@ void deleteProcess(char** backProc, pid_t ** id, char* procToRem, int size, int*
 	int i;
 	for (i = 0; i < *current; i++)
 	{
-		if (strcmp(backProc[i],procToRem) == 0 && 
+		if (strcmp(backProc[i],procToRem) == 0 &&
 		    strcmp(backProc[i],"NULL") != 0)//found the process to remove
 		{
 			backProc[i] = backProc[i+1];	//remove command and id from both arrays by shifting
@@ -1212,7 +1159,7 @@ void deleteProcess(char** backProc, pid_t ** id, char* procToRem, int size, int*
 }
 
 // determine if process done, then will remove
-int processEnd(char** backProc, pid_t** id, int size, int* current)		
+int processEnd(char** backProc, pid_t** id, int size, int* current)
 {
 	int status;
 	int i;
@@ -1228,4 +1175,65 @@ int processEnd(char** backProc, pid_t** id, int size, int* current)
 		}}
 	}
 	return  retVal;
+}
+
+    /* extra functions */
+char* resizeTeacher(char* his, char* ours) // will resize the current in instruction struct
+{
+	free(his);
+        his = (char*)malloc(strlen(ours) * sizeof(char));
+        his = ours;
+        ours = NULL;
+	return his;
+}
+
+// releases memory of dynamically allocated array
+void deallocateArray(char** arr, int size) {
+	int i;
+	for (i = 0; i < size; i++)
+		free(arr[i]);
+
+	free(arr);
+}
+
+// create new dynamic array of size 'size'
+char** createArray(int size) {
+	char** newarr = (char**)malloc(size * sizeof(char*));	// size of array
+
+	int i;
+	for (i = 0; i < size; i++) 			// size of each index
+		newarr[i] = (char*)malloc(100 * sizeof(char));
+
+
+	return newarr;
+}
+
+// resizes an array and changes the size to double the passed size
+char** resizeArray(char** array, int* sizeofarray) {
+
+	// keep origional size for dealocation, double it for new array
+	int oldsize = *sizeofarray;
+	*sizeofarray = oldsize * 2;
+
+	// create new array
+	char ** new = (char**)malloc(*sizeofarray * sizeof(char*));
+
+	// allocate each index of new array
+	int i;
+	for (i = 0; i < *sizeofarray; i++)
+		new[i] = (char*)malloc(100 * sizeof(char));
+
+
+	// copy contents from old to new
+	for (i = 0; i < oldsize; i++)
+		strcpy(new[i], array[i]);
+
+
+	// free old array
+	for (i = 0; i < oldsize; i++)
+		free(array[i]);
+
+	free(array);
+
+	return new;
 }
